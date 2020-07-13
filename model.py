a3d3a83e6ac688658ab223cc7565b7780fd7289a
Ahmet Berk Bektaş <ahmetberkbektas@gmail.com>
2020-07-14 00:12:16 +0300

    Update model.py

--- model.py
+++ model.py
@@ -2,17 +2,16 @@
 
 import numpy as np 
 import pandas as pd 
-from tensorflow import keras
-from keras.layers import Conv2D,MaxPool2D,Dense,Dropout,Flatten
-from keras.models import Sequential
-from keras.preprocessing.image import ImageDataGenerator
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from keras.utils.np_utils import to_categorical
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sklearn.metrics import confusion_matrix
+from tensorflow import keras
+from keras.layers import Conv2D, MaxPool2D, Dense, Dropout, Flatten
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from keras.utils.np_utils import to_categorical
 
 data = pd.read_csv("hmnist_64_64_L.csv") 
 
@@ -22,6 +21,7 @@ X = data
 
 img = X.iloc[75].as_matrix()
 img = img.reshape(64,64)
+
 X = X.values.reshape(-1,64,64,1)
 Y = Y.values
 Y = to_categorical(Y)
@@ -62,21 +62,29 @@ datagen = ImageDataGenerator(
         width_shift_range=0.5,  
         height_shift_range=0.5, 
         horizontal_flip=True, 
-        vertical_flip=True)
+        vertical_flip=True,
+)
 
 datagen.fit(x_train)
 
-model.fit_generator(datagen.flow(x_train,y_train, batch_size=200),
-                              epochs = 50, validation_data = (x_test,y_test), steps_per_epoch=200)
+model.fit_generator(
+        datagen.flow(x_train,y_train, batch_size=200),
+        epochs = 50,
+        validation_data = (x_test,y_test),
+        steps_per_epoch=200,
+)
 
 Y_pred = model.predict(x_test)
 Y_pred_classes = np.argmax(Y_pred,axis = 1) 
-Y_true = np.argmax(y_test,axis = 1) 
+Y_true_classes = np.argmax(y_test,axis = 1)
 print(classification_report(Y_true, Y_pred_classes))
+
 confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
 f,ax = plt.subplots(figsize=(18, 16))
-sns.heatmap(confusion_mtx, annot=True, linewidths=0.01,cmap="summer_r", fmt= '.1f',ax=ax)
+sns.heatmap(confusion_mtx, annot=True, linewidths=0.01, cmap="summer_r", fmt='.1f', ax=ax)
+
 plt.xlabel("Predicted Label")
 plt.ylabel("True Label")
 plt.title("Confusion Matrix")
+
 plt.show()
